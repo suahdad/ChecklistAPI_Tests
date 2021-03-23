8391794a555c7d5dac944794a58bf7ef19f756a7
jpasol <38642594+jpasol@users.noreply.github.com>
2021-03-23 13:54:59 +0800

    Update VouchersControllerTests.cs

--- VouchersControllerTests.cs
+++ VouchersControllerTests.cs
@@ -46,10 +46,46 @@ namespace ChecklistAPI_Tests
             _context.Vouchers.Add(_voucher);
             _context.SaveChanges();
 
-            _controller.PostVoucher(_voucher);
+            await _controller.PostVoucher(new Voucher() {
+                EquipmentID = "PM01",
+                UserID = "TEST"
+            });
             var _result = _controller.GetVouchers().Result.Value.ToArray();
 
-            Assert.NotEqual(_voucher.Validity, _result[0].Validity);
+            Assert.NotEqual(_voucher.Validity, ((Voucher)_result[0]).Validity);
+        }
+
+        [Fact]
+        public async Task ShouldDeleteRelatedVouchersAtPost()
+        {
+            var _vouchers = new Voucher[] {
+                new Voucher
+                {
+                    EquipmentID = "PM01",
+                    UserID = "TEST"
+                },
+                new Voucher
+                {
+                    EquipmentID = "SL01",
+                    UserID = "TEST"
+                }
+
+            };
+
+            var _context = new SampleDBContext(useSQLLite: true).Context;
+            var _controller = new VouchersController(_context);
+
+            _context.Vouchers.AddRange(_vouchers);
+            _context.SaveChanges();
+
+            await _controller.PostVoucher(new Voucher()
+            {
+                EquipmentID = "PM01",
+                UserID = "TEST"
+            });
+            var _result = _controller.GetVouchers().Result.Value.ToArray();
+
+            Assert.Single(_result);
         }
     }
 }
